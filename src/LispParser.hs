bfc2d7ad061009a675d13c1d19ec19ffa383a795
akthrms <akt.hrms@gmail.com>
2021-07-02 12:34:08 +0900

    update

--- src/LispParser.hs
+++ src/LispParser.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module LispParser (runRepl, evalAndPoint) where
 
 import Control.Monad.Except
@@ -43,6 +45,8 @@ instance Show LispError where
   show (UnboundVar message varName) = message ++ ": " ++ varName
 
 type ThrowsError = Either LispError
+
+data Unpacker = forall a. Eq a => AnyUnpacker (LispValue -> ThrowsError a)
 
 -- Parsers
 
@@ -106,6 +110,11 @@ eval val@(String _) = pure val
 eval val@(Number _) = pure val
 eval val@(Bool _) = pure val
 eval (List [Atom "quote", val]) = pure val
+eval (List [Atom "if", pred, thenVal, elseVal]) = do
+  result <- eval pred
+  case result of
+    Bool False -> eval elseVal
+    _ -> eval thenVal
 eval (List (Atom fun : args)) = mapM eval args >>= apply fun
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
@@ -124,12 +133,40 @@ primitives =
     ("/", numBinOperation div),
     ("mod", numBinOperation mod),
     ("quotient", numBinOperation quot),
-    ("remainder", numBinOperation rem)
+    ("remainder", numBinOperation rem),
+    ("=", boolBinOperation unpackNum (==)),
+    ("<", boolBinOperation unpackNum (<)),
+    (">", boolBinOperation unpackNum (>)),
+    ("/=", boolBinOperation unpackNum (/=)),
+    ("<=", boolBinOperation unpackNum (<=)),
+    (">=", boolBinOperation unpackNum (>=)),
+    ("&&", boolBinOperation unpackBool (&&)),
+    ("||", boolBinOperation unpackBool (||)),
+    ("string=?", boolBinOperation unpackString (==)),
+    ("string<?", boolBinOperation unpackString (<)),
+    ("string>?", boolBinOperation unpackString (>)),
+    ("string<=?", boolBinOperation unpackString (<=)),
+    ("string>=?", boolBinOperation unpackString (>=)),
+    ("car", car),
+    ("cdr", cdr),
+    ("cons", cons),
+    ("eq?", eqv),
+    ("eqv?", eqv),
+    ("equal?", equal)
   ]
 
 numBinOperation :: (Integer -> Integer -> Integer) -> [LispValue] -> ThrowsError LispValue
-numBinOperation operation param@[_] = throwError $ NumArgs 2 param
-numBinOperation operation params = Number . foldl1 operation <$> mapM unpackNum params
+numBinOperation operation arg@[_] = throwError $ NumArgs 2 arg
+numBinOperation operation args = Number . foldl1 operation <$> mapM unpackNum args
+
+boolBinOperation :: (LispValue -> ThrowsError a) -> (a -> a -> Bool) -> [LispValue] -> ThrowsError LispValue
+boolBinOperation unpacker operation args =
+  if length args /= 2
+    then throwError $ NumArgs 2 args
+    else do
+      left <- unpacker $ head args
+      right <- unpacker $ args !! 1
+      pure $ Bool $ left `operation` right
 
 unpackNum :: LispValue -> ThrowsError Integer
 unpackNum (Number n) = pure n
@@ -141,6 +178,73 @@ unpackNum (String n) =
     parsed = reads n
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum = throwError $ TypeMismatch "number" notNum
+
+unpackString :: LispValue -> ThrowsError String
+unpackString (String s) = pure s
+unpackString (Number s) = pure $ show s
+unpackString (Bool s) = pure $ show s
+unpackString notString = throwError $ TypeMismatch "string" notString
+
+unpackBool :: LispValue -> ThrowsError Bool
+unpackBool (Bool b) = pure b
+unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
+
+car :: [LispValue] -> ThrowsError LispValue
+car [List (x : _)] = pure x
+car [DottedList (x : _) _] = pure x
+car [badArgs] = throwError $ TypeMismatch "pair" badArgs
+car badArgs = throwError $ NumArgs 1 badArgs
+
+cdr :: [LispValue] -> ThrowsError LispValue
+cdr [List (_ : xs)] = pure $ List xs
+cdr [DottedList [_] x] = pure x
+cdr [DottedList (_ : xs) x] = pure $ DottedList xs x
+cdr [badArgs] = throwError $ TypeMismatch "pair" badArgs
+cdr badArgs = throwError $ NumArgs 1 badArgs
+
+cons :: [LispValue] -> ThrowsError LispValue
+cons [x, List []] = pure $ List [x]
+cons [x, List xs] = pure $ List $ x : xs
+cons [x, DottedList xs y] = pure $ DottedList (x : xs) y
+cons [x, y] = pure $ DottedList [x] y
+cons badArgs = throwError $ NumArgs 2 badArgs
+
+eqv :: [LispValue] -> ThrowsError LispValue
+eqv [Bool arg1, Bool arg2] = pure $ Bool $ arg1 == arg2
+eqv [Number arg1, Number arg2] = pure $ Bool $ arg1 == arg2
+eqv [String arg1, String arg2] = pure $ Bool $ arg1 == arg2
+eqv [Atom arg1, Atom arg2] = pure $ Bool $ arg1 == arg2
+eqv [DottedList xs x, DottedList ys y] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [List args1, List args2] =
+  pure $ Bool $ (length args1 == length args2) && all eqvPair (zip args1 args2)
+  where
+    eqvPair (x, y) = case eqv [x, y] of
+      Left err -> False
+      Right (Bool val) -> val
+eqv [_, _] = pure $ Bool False
+eqv badArgs = throwError $ NumArgs 2 badArgs
+
+unpackEquals :: LispValue -> LispValue -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do
+    unpacked1 <- unpacker arg1
+    unpacked2 <- unpacker arg2
+    pure $ unpacked1 == unpacked2
+    `catchError` const (pure False)
+
+equal :: [LispValue] -> ThrowsError LispValue
+equal [arg1, arg2] = do
+  primitiveEquals <-
+    or
+      <$> mapM
+        (unpackEquals arg1 arg2)
+        [ AnyUnpacker unpackNum,
+          AnyUnpacker unpackString,
+          AnyUnpacker unpackBool
+        ]
+  eqvEquals <- eqv [arg1, arg2]
+  pure $ Bool $ primitiveEquals || let (Bool x) = eqvEquals in x
+equal badArgs = throwError $ NumArgs 2 badArgs
 
 -- IO
 
